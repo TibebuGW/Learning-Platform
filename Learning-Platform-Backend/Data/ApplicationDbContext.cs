52534398f7be54e2c7ae89b69a75afc84fe93271
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2019-05-12 12:07:32 +0300

    feat(backend): applicationdbcontext.cs added

--- Learning-Platform-Backend/Data/ApplicationDbContext.cs
+++ Learning-Platform-Backend/Data/ApplicationDbContext.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Teaching.Models;
-
 namespace Teaching.Data
 {
     public class ApplicationDbContext: DbContext
